6e90d5522030c90552ccc820fca35821fcf48639
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2023-11-28 17:24:20 +0530

    Server

--- nodejs/server/index.js
+++ nodejs/server/index.js
@@ -1,12 +1,41 @@
 const http = require('http');
 
-const PORT = 3022;
+const PORT = 3030;
 const HOSTNAME = 'localhost';
 
 const server = http.createServer((req, res) =>{
-    res.statusCode = 500;
-    res.setHeader('Context-Type', 'application/json')
-    res.end('Node server created by Himanshu Yadav!')
+
+    //Home Page
+    // About PAge
+    // Conatact PAge
+    // Product PAge
+    // Rest... > Error
+
+    if (req.url == '/'){
+        res.statusCode = 200;
+        res.setHeader('Context-Type', 'text/plain')
+        res.end("Welcome to Node JS Server Home") 
+        
+    }else if (req.url == '/about'){
+        res.statusCode = 200;
+        res.setHeader('Context-Type', 'text/plain')
+        res.end("Welcome to Node JS Server About")
+        
+    } else if (req.url == '/contact'){
+        res.statusCode = 200;
+        res.setHeader('Context-Type', 'text/plain')
+        res.end("Welcome to Node JS Server Conatact")
+    } else if (req.url == '/product'){
+        res.statusCode = 200;
+        res.setHeader('Context-Type', 'application/json')
+        res.end(JSON.stringify({productName: "Product 1"})) 
+    } else {
+        res.statusCode = 500;
+        res.setHeader('Context-Type', 'text/plain')
+        res.end('Error!')
+    }
+
+    
 });
 
 server.listen(PORT, () => {
--- nodejs/server/request.js
+++ nodejs/server/request.js
@@ -1,6 +1,20 @@
 const http = require('http');
 
+const Option = {
+    hostname: 'fakeapi.com',
+    path: '/product/1',
+    method: 'GET'
+}
 
 
+const apiReq = http.request(Option, (apiRes) => {
+    apiRes.on("data", (data) => {
+        console.log(data.tostring());
+    })
+});
 
-const req = http.request(Option)
+apiReq.on("error", () => {
+    console.log(e);
+});
+
+apiReq.end();
